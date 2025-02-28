72e69db47df09275538f5b5c117cd2e4f62c0754
nikhil-1e9 <relativity1999@gmail.com>
2025-03-01 02:34:39 +0530

    Remove signup from quiz/urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.quiz_page, name='quiz_page'),
     path('question/<int:question_id>/', views.question_detail, name='question_detail'),
-    path('signup/', views.signup, name='signup'),  # Signup URL
+    # path('signup/', views.signup, name='signup'),  # Signup URL
 ]
